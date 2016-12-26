50a332e8a83c9ffa6e976d669dd6a30e71fbeada
Ying Zhang <ying.zhang.zoe@gmail.com>
2016-12-25 21:58:51 -0600

    ExData project 1 12.25.2016

---
+++ plot3.R
@@ -0,0 +1,27 @@
+# download and unzip data
+setwd("E:/Dropbox/Study/Datascience certification/Exploratory Data/project 1")
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip('./data/household_power_consumption.zip', exdir = './data')
+
+# read data into R
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+# Plot 3
+# convert data and time to specific format
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot3.png', width = 480, height = 480, units='px')
+# plot figure
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+# close device
+dev.off()
+
---
+++ plot4.R
@@ -0,0 +1,30 @@
+# download and unzip data
+setwd("E:/Dropbox/Study/Datascience certification/Exploratory Data/project 1")
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip('./data/household_power_consumption.zip', exdir = './data')
+
+# read data into R
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+# Plot 4
+# convert data and time to specific format
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot4.png', width = 480, height = 480, units='px')
+# plot figure
+Sys.setlocale(category = "LC_ALL", locale = "english")
+par(mfrow = c(2, 2))
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(data$DateTime, data$Voltage, xlab = 'datetime', ylab = 'Voltage', type = 'l')
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+plot(data$DateTime, data$Global_reactive_power, xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
+# close device
+dev.off()
